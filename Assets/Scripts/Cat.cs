f1ce07d148a2db7da632c52a74f0b1898c781822
Jenny Jiao Hsia <jj1317@nyu.edu>
2015-11-02 11:32:55 -0500

    lil pear dude

---
+++ Assets/Scripts/Cat.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cat : MonoBehaviour {
+
+	// declare a public variable, of type Transform, called "mouse"
+	public Transform mouse;
+
+	Rigidbody rb;
+	
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+	}
+	
+
+	void FixedUpdate () {
+
+		if (mouse == null) {
+			return;
+		}
+
+		// declare a var of type Vector3, called "directionToMouse", 
+		// set to a vector that goes from [current position] to [mouse's current position]
+		Vector3 directionToMouse = mouse.position - transform.position;
+		float distanceToMouse = Vector3.Distance(mouse.position, transform.position);
+
+		// if the angle between [current forward direction] vs [directionToMouse] is less than 90 degrees,
+
+		float angle = Vector3.Angle (transform.forward, directionToMouse); 
+		if (angle < 90f  || distanceToMouse < 5f) {
+
+			// then declare a var of type Ray, called "catRay" that starts from
+			// current position and goes along [directionToMouse]
+			Ray catRay = new Ray(transform.position, directionToMouse); 
+
+			// declare a var of type RaycastHit, called "catRayHitInfo"
+			RaycastHit catRayHitInfo = new RaycastHit(); 
+
+			// if raycast with catRay and catRayHitInfo for 100 units is TRUE... 
+			if (Physics.Raycast (catRay, out catRayHitInfo, 100f) ) {
+
+				// if catRayHitInfo.collider.tag is exactly equal to the word "Mouse" (cat sees the mouse!)
+				if (catRayHitInfo.collider.tag == "Mouse" ) {
+
+					
+					// if catRayHitInfo.distance is less than or equal to 5... 
+					if (catRayHitInfo.distance <= 3f) {
+
+						// then destroy the mouse gameObject (we caught the mouse!) 
+						Destroy(mouse.gameObject);
+
+					} else {
+						// add force on rigidbody, along [directionToMouse.normalized * 1000f] (chase it!)
+						rb.AddForce (directionToMouse.normalized * 1000f);
+					}
+				}
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+	Rigidbody rb;
+
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+	}
+
+	void FixedUpdate () {
+		// set rigidbody velocity equal to [current forward direction] * 10f + Physics.gravity 
+		rb.velocity = transform.forward * 10f + Physics.gravity;
+
+		// declare a var of type Ray, called "moveRay" that starts from [current position] 
+		// and goes [current forward direction]
+
+		Ray moveRay = new Ray ( transform.position, transform.forward );
+
+		// if Raycast with moveRay for 3 units is TRUE... (if there is an obstacle in front of us...)
+		// then randomly turn 90 degrees left or right (around Y axis) 
+		//if ( Physics.Raycast(moveRay, 3f) ) {
+		if (Physics.SphereCast (moveRay, 0.9f, 2f) ) {
+			int randomNum = Random.Range (0,2); // returns either 0 or 1
+			if (randomNum == 0) { // turn right
+				transform.Rotate (0f, 90f, 0f);
+
+			} else if (randomNum == 1) { // turn left
+				transform.Rotate (0f, -90f, 0f);
+
+			}
+		} 
+
+	}
+	
+}
